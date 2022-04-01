2927272886d846654f57edb4fdf9cd7fed4b5c36
Onur Çelikler <onrclklr@gmail.com>
2022-04-01 17:34:30 +0300

    Removed Headers

--- tierlists.py
+++ tierlists.py
@@ -3,13 +3,9 @@ import requests
 
 url = "https://tr.op.gg/champion/statistics"
 
-headers = {
-    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 OPR/80.0.4170.91'
-}
-
 data = []
 
-result = requests.get(url, headers=headers)
+result = requests.get(url)
 doc= BeautifulSoup(result.content, "html.parser")
 
 # Getting Top Lane Tier List
